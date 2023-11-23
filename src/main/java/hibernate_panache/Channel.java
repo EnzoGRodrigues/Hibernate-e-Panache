892bbde9f6de5bb3896fb4762bb28296c28497a1
Enzo Rodrigues <enzorodrigues272@icloud.com>
2023-11-22 22:41:45 -0300

    primeiro commit

---
+++ src/main/java/hibernate_panache/Channel.java
@@ -0,0 +1,52 @@
+package hibernate_panache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+
+
+@Entity
+public class Channel extends PanacheEntity {
+    
+    private String hash;
+
+    @ManyToMany(mappedBy = "channels", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<User> users;
+
+    public Channel() {
+    }
+
+    public Channel(String hash) {
+        this.users = new ArrayList<>();
+        this.setHash(hash);
+    }
+
+    public String getHash() {
+        return this.hash;
+    }
+
+
+    private void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public List<User> getUsers() {
+        return this.users;
+    }
+
+    public void setUser(User user) {
+        this.users.add(user);
+    }
+
+    @Override
+    public String toString() {
+        return "Channel: " + this.getHash();
+    }
+}
---
+++ src/main/java/hibernate_panache/User.java
@@ -0,0 +1,71 @@
+package hibernate_panache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+
+@Entity
+public class User extends PanacheEntity{
+
+    private String name;
+    
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    // name = nome da coluna que irá armazenar a chave estrangeira
+    // na tabela Message (solução a partir da JPA 2)
+    @JoinColumn(name = "users_id")
+    private List<Message> messages;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<Channel> channels;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.messages = new ArrayList<>();
+        this.channels = new ArrayList<>();
+        this.setName(name);
+    }
+
+
+    public String getName() {
+        return this.name;
+    }   
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Message> getMessages() {
+        return this.messages;
+    }
+
+    public void setMessage(Message message) {
+        this.messages.add(message);
+    }
+
+    public List<Channel> getChannels() {
+        return this.channels;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channels.add(channel);
+    }
+
+    @Override
+    public String toString() {
+        return "User: "  + this.getName();
+    }
+    
+}
